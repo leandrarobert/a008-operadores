b0bc1ca4f3b7848d8f2c14b63d9f0532825f8c20
leandrarobert <leandraroberta234@gmail.com>
2022-08-30 22:36:24 -0300

    atividade

---
+++ exercicio1/script.js
@@ -0,0 +1,3 @@
+let numeropar = Number (prompt ("Insira um numero par"))
+let divisao = numeropar % 2
+console.log (`O resto da divisão do número ${numeropar} pelo número 2 é ${divisao}`) 
---
+++ exercicio2/script.js
@@ -0,0 +1,9 @@
+let a = Number (prompt ("Digite o primeiro numero"))
+let b = Number (prompt ("Insira o segundo numero"))
+console.log (`${a > b}`)
+
+console.log (`${a === b}`)
+
+console.log (`${a % b===0}`)
+
+console.log (`${b % a===0}`)
